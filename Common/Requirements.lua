8b24b71ec32374103674fa5188515eae07d7f9b0
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2024-10-29 21:42:38 +0100

    Update Requirements.lua

--- Common/Requirements.lua
+++ Common/Requirements.lua
@@ -1,42 +1,47 @@
+type RequirementsModule = {
+	Call : (functionName : string, ...any) -> any?,
+	IsCompatible : () -> (boolean, {string})
+}
+
 local requiredFunctions = {
-    ["CloneRef"] = cloneref,
-    ["IsFolder"] = isfolder,
-    ["ReadFile"] = readfile,
-    ["DelFolder"] = delfolder,
-    ["WriteFile"] = writefile,
-    ["MakeFolder"] = makefolder,
-    ["NewCClosure"] = newcclosure,
-    ["SetReadOnly"] = setreadonly,
-    ["GetMetatable"] = getrawmetatable or debug.getmetatable,
-    ["HookFunction"] = hookfunction or detour_function,
-    ["GetConnections"] = get_signal_cons or getconnections,
-    ["HookMetamethod"] = hookmetamethod or (hookFunction and function(instance : Instance, method : string, newFunction : (...any) -> (...any))
-        local metatable = getrawmetatable(instance) or debug.getmetatable(instance)
-        setreadonly(metatable, false)
-        return hookfunction(metatable[method], newcclosure(newFunction))
-    end),
+	["CloneRef"] = cloneref,
+	["IsFolder"] = isfolder,
+	["ReadFile"] = readfile,
+	["DelFolder"] = delfolder,
+	["WriteFile"] = writefile,
+	["MakeFolder"] = makefolder,
+	["NewCClosure"] = newcclosure,
+	["SetReadOnly"] = setreadonly,
+	["GetMetatable"] = getrawmetatable or debug.getmetatable,
+	["HookFunction"] = hookfunction or detour_function,
+	["GetConnections"] = get_signal_cons or getconnections,
+	["HookMetamethod"] = hookmetamethod or (hookFunction and function(instance : Instance, method : string, newFunction : (...any) -> (...any))
+		local metatable = getrawmetatable(instance) or debug.getmetatable(instance)
+		setreadonly(metatable, false)
+		return hookfunction(metatable[method], newcclosure(newFunction))
+	end),
 }
 
 local Requirements = {}
 
 function Requirements:Call(functionName : string, ... : any) : any?
-    local functionValue = requiredFunctions[functionName] or getgenv()[functionName]
-    if functionValue then
-        return functionValue(...)
-    end
-    return nil
+	local functionValue = requiredFunctions[functionName] or getgenv()[functionName]
+	if functionValue then
+		return functionValue(...)
+	end
+	return nil
 end
 
-function Requirements:IsCompatible() : boolean
-    local isCompatible = true
-    local unsupportedFunctions = {}
-    for functionName, functionValue in requiredFunctions do
-        if typeof(functionValue) ~= "function" then
-            isCompatible = false
-            table.insert(unsupportedFunctions, string.lower(functionName))
-        end
-    end
-    return isCompatible, unsupportedFunctions
+function Requirements:IsCompatible() : (boolean, {string})
+	local isCompatible = true
+	local unsupportedFunctions = {}
+	for functionName, functionValue in requiredFunctions do
+		if typeof(functionValue) ~= "function" then
+			isCompatible = false
+			table.insert(unsupportedFunctions, string.lower(functionName))
+		end
+	end
+	return isCompatible, unsupportedFunctions
 end
 
 return Requirements
