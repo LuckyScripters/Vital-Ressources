d1c4954b88a3308477e7edd07540b9024ceb3825
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2024-11-12 22:21:27 +0100

    Update ThemeManager.lua

--- UILibrary/ThemeManager.lua
+++ UILibrary/ThemeManager.lua
@@ -47,7 +47,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'Vital'
+		local theme = 'Default'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
