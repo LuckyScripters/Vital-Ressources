02be9ea5b6e6d8b5e678dbef620854ca7fce12e4
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2025-01-08 21:18:20 +0100

    Update ConfigsManager.lua

--- Libraries/ConfigsManager.lua
+++ Libraries/ConfigsManager.lua
@@ -1,6 +1,7 @@
 type ConfigManager = {
 	__index : ConfigManager,
-	new : () -> ConfigSettings,
+	new : (name : string) -> ConfigSettings?,
+	LoadMainFolder : () -> (),
 	AddConfig : (self : ConfigSettings, configName : string, config : {[string] : any}) -> (),
 	GetConfig : (self : ConfigSettings, configName : string) -> {[string] : any}?,
 	ModifyConfig : (self : ConfigSettings, configName : string, key : string, value : any) -> boolean,
@@ -8,18 +9,37 @@ type ConfigManager = {
 }
 
 type ConfigSettings = typeof(setmetatable({} :: {
+	Name : string,
 	Configs : {[string] : {[string] : any}},
 	Listeners : {[string] : (configName : string, key : string, newValue : any, oldValue : any) -> ()}
 }, {} :: ConfigManager))
 
-local ConfigManager : ConfigManager = {} :: ConfigManager
+local ConfigManager : ConfigManager = {MainFolderName = "VitalMainSettings", IsMainFolderLoaded = false} :: ConfigManager
 ConfigManager.__index = ConfigManager
 
-function ConfigManager.new() : ConfigSettings
+function ConfigManager.new(name : string) : ConfigSettings
+	if not ConfigManager.IsMainFolderLoaded then
+		warn("Main folder not loaded, you should load it before creating settings")
+		return nil
+	end
 	local self = setmetatable({}, ConfigManager)
+	self.Name = name
 	self.Configs = {}
 	self.Listeners = {}
+	if not isfolder(ConfigManager.MainFolderName .. "/" .. name) then
+		makefolder(ConfigManager.MainFolderName .. "/" .. name)
+	end
 	return self
+end
+
+function ConfigManager:LoadMainFolder()
+	if ConfigManager.IsMainFolderLoaded then
+		return
+	end
+	ConfigManager.IsMainFolderLoaded = true
+	if not isfolder(configManager.MainFolderName) then
+		makefolder(configManager.MainFolderName)
+	end
 end
 
 function ConfigManager:GetConfig(configName : string) : {[string] : any}?
@@ -31,6 +51,7 @@ function ConfigManager:AddConfig(configName : string, config : {[string] : any})
 		warn("Configuration with this name already exists: " .. configName)
 		return
 	end
+	makefolder(ConfigManager.MainFolderName .. "/" .. self.Name .. "/" .. configName)
 	self.Configs[configName] = config
 end
 
