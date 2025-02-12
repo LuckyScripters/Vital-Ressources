299cf68425905e9961827ccea3e3f6d34f2574c9
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2025-02-12 22:54:15 +0100

    Update Library.lua

--- UILibrary/Library.lua
+++ UILibrary/Library.lua
@@ -788,10 +788,10 @@ library.createSlider = function(option, parent)
         value = library.round(value, option.float)
         value = math.clamp(value, self.min, self.max)
         if self.min >= 0 then
-            option.fill:TweenSize(UDim2.new((value - self.min) / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.05, true)
+            --option.fill:TweenSize(UDim2.new((value - self.min) / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.05, true)
         else
-            option.fill:TweenPosition(UDim2.new((0 - self.min) / (self.max - self.min), 0, 0, 0), "Out", "Quad", 0.05, true)
-            option.fill:TweenSize(UDim2.new(value / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.1, true)
+            --option.fill:TweenPosition(UDim2.new((0 - self.min) / (self.max - self.min), 0, 0, 0), "Out", "Quad", 0.05, true)
+            --option.fill:TweenSize(UDim2.new(value / (self.max - self.min), 0, 1, 0), "Out", "Quad", 0.1, true)
         end
         library.flags[self.flag] = value
         self.value = value
